ef271de14d80b88cd5200a540239f330e67ff12a
David Hinojosa <davidhinojosauni@gmail.com>
2021-03-04 16:12:47 -0600

    Last version no np attempt

--- gauss.py
+++ gauss.py
@@ -2,13 +2,44 @@
 
 import numpy as np
 
-# Inicializar variables 'x' 'y' y 'z'
-x = 0
-y = 0
-z = 0
 
-# Cantidad de iteraciones
-n = 10
 
-for i in range(n):
-    pass
+
+# Ask for system of equations
+def SystemEquations():  
+    N = int(input("Cantidad de ecuaciones en el sistema: "))
+    sysEq = []  
+
+    for i in range(N):
+        print("Ecuacion", i+1)
+        sysEq.append([])
+        for j in range(N):
+            x = int(input("Constante de x" + str(j+1) + ": "))
+
+            sysEq[i].append(x)
+            print(sysEq)
+
+    return sysEq
+
+def SystemZero():  
+    N = int(input("Cantidad de ecuaciones en el sistema: "))
+    sysZero = []  
+
+    for i in range(N):
+        print("Ecuacion", i+1)
+        sysZero.append([])
+        for j in range(N):
+            x = 0
+
+            sysZero[i].append(x)
+
+    return sysZero
+
+def dot(K, L):
+   if len(K) != len(L):
+      return 0
+
+   return sum(i[0] * i[1] for i in zip(K, L))
+
+
+
